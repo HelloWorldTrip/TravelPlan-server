7aa37a6eacd1e887bbdf9d4534b2fa41719bfa09
bckim9489 <bckim9489@gmail.com>
2024-06-23 01:56:13 +0900

    [fect] MemberEntity 작성

--- src/main/java/com/HelloWorldTrip/TravelPlan/domain/MemberEntity.java
+++ src/main/java/com/HelloWorldTrip/TravelPlan/domain/MemberEntity.java
@@ -1,20 +1,35 @@
 package com.HelloWorldTrip.TravelPlan.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
+import jakarta.persistence.*;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+
+import java.time.LocalDateTime;
+
 @Entity
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
-@ToString
+@Table(name = "member_tbl")
 public class MemberEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="user_id")
+    private String userId;
+
+    @Column(name="user_name")
+    private String userName;
+
+    @Column(name="user_pw")
+    private String userPw;
+
+    @Column(name="use_yn")
+    private String useYn;
+
+    @Column(name = "frst_date", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime frstDate;
+
+    @Column(name = "last_date", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime lastDate;
 }

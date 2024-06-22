07d5732b23653149c37d88f4f16387f668c2e23a
bckim9489 <bckim9489@gmail.com>
2024-06-23 01:57:46 +0900

    [fix] MemberEntityRepository 메소드 추가(findByUserId)

--- src/main/java/com/HelloWorldTrip/TravelPlan/repository/MemberEntityRepository.java
+++ src/main/java/com/HelloWorldTrip/TravelPlan/repository/MemberEntityRepository.java
@@ -4,6 +4,9 @@ import com.HelloWorldTrip.TravelPlan.domain.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberEntityRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByUserId(String userId);
 }

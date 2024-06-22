8e1c79afac0d3ea6e62185ad2e586c0a9d1826b4
bckim9489 <bckim9489@gmail.com>
2024-06-23 01:56:47 +0900

    [fect] MemberDTO 작성

---
+++ src/main/java/com/HelloWorldTrip/TravelPlan/dto/MemberDTO.java
@@ -0,0 +1,10 @@
+package com.HelloWorldTrip.TravelPlan.dto;
+import lombok.Data;
+
+@Data
+public class MemberDTO {
+    private String userId;
+    private String userName;
+    private String userPw;
+    private String useYn;
+}

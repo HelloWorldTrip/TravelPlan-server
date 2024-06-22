656a83586992629d53abb3c16ec4aa96a133f796
bckim9489 <bckim9489@gmail.com>
2024-06-23 01:55:09 +0900

    [fect] MemberMapper 추가

--- src/main/java/com/HelloWorldTrip/TravelPlan/mapper/MemberMapper.java
+++ src/main/java/com/HelloWorldTrip/TravelPlan/mapper/MemberMapper.java
@@ -3,7 +3,6 @@ package com.HelloWorldTrip.TravelPlan.mapper;
 import com.HelloWorldTrip.TravelPlan.domain.MemberEntity;
 import com.HelloWorldTrip.TravelPlan.dto.MemberDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper

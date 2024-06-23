0645fdc466968bfa846060faeab335f2209f1ca9
bckim9489 <bckim9489@gmail.com>
2024-06-23 21:07:14 +0900

    [fect] 사용자 역할 추가

--- src/main/java/com/HelloWorldTrip/TravelPlan/domain/MemberEntity.java
+++ src/main/java/com/HelloWorldTrip/TravelPlan/domain/MemberEntity.java
@@ -3,14 +3,19 @@ package com.HelloWorldTrip.TravelPlan.domain;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Data
 @NoArgsConstructor
 @Table(name = "member_tbl")
-public class MemberEntity {
+public class MemberEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer uid;
@@ -24,6 +29,9 @@ public class MemberEntity {
     @Column(name="user_pw")
     private String userPw;
 
+    @Column(name = "user_role", nullable = false)
+    private String role;
+
     @Column(name="use_yn")
     private String useYn;
 
@@ -32,4 +40,39 @@ public class MemberEntity {
 
     @Column(name = "last_date", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime lastDate;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+
+    @Override
+    public String getPassword() {
+        return userPw;
+    }
+
+    @Override
+    public String getUsername() {
+        return userId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return "Y".equals(useYn);
+    }
 }

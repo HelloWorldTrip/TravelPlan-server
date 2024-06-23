24261b7e80507109f3c1ab3bf0d90f96ee5828ff
bckim9489 <bckim9489@gmail.com>
2024-06-23 16:07:37 +0900

    [refactor] API Message 형식 변경(status code 포함하도록)

--- src/main/java/com/HelloWorldTrip/TravelPlan/enums/ApiMessage.java
+++ src/main/java/com/HelloWorldTrip/TravelPlan/enums/ApiMessage.java
@@ -1,31 +1,36 @@
 package com.HelloWorldTrip.TravelPlan.enums;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ApiMessage {
     // Success Messages
-    MEMBERS_RETRIEVED_SUCCESS("SUCCESS_001", "Members retrieved successfully"),
-    MEMBER_RETRIEVED_SUCCESS("SUCCESS_002", "Member retrieved successfully"),
-    MEMBER_REGISTERED_SUCCESS("SUCCESS_003", "Member registered successfully"),
-    PASSWORD_CHANGED_SUCCESS("SUCCESS_004", "Password changed successfully"),
-    MEMBER_DEACTIVATED_SUCCESS("SUCCESS_005", "Member deactivated successfully"),
+    MEMBERS_RETRIEVED_SUCCESS("Members retrieved successfully", HttpStatus.OK),
+    MEMBER_RETRIEVED_SUCCESS("Member retrieved successfully", HttpStatus.OK),
+    MEMBER_REGISTERED_SUCCESS("Member registered successfully", HttpStatus.CREATED),
+    MEMBER_UPDATED_SUCCESS("Member updated successfully", HttpStatus.OK),
+    PASSWORD_CHANGED_SUCCESS("Password changed successfully", HttpStatus.OK),
+    MEMBER_DEACTIVATED_SUCCESS("Member deactivated successfully", HttpStatus.OK),
 
     // Error Messages
-    MEMBER_NOT_FOUND("ERROR_001", "Member not found"),
-    USER_NOT_FOUND("ERROR_002", "User not found"),
-    INTERNAL_SERVER_ERROR("ERROR_003", "An internal server error occurred"),
-    FAILED_TO_RETRIEVE_MEMBERS("ERROR_004", "Failed to retrieve members"),
-    FAILED_TO_REGISTER_MEMBER("ERROR_005", "Failed to register member"),
-    FAILED_TO_CHANGE_PASSWORD("ERROR_006", "Failed to change password"),
-    FAILED_TO_DEACTIVATE_MEMBER("ERROR_007", "Failed to deactivate member");
+    MEMBER_NOT_FOUND("Member not found", HttpStatus.NOT_FOUND),
+    USER_NOT_FOUND("User not found", HttpStatus.NOT_FOUND),
+    INTERNAL_SERVER_ERROR("An internal server error occurred", HttpStatus.INTERNAL_SERVER_ERROR),
+    FAILED_TO_RETRIEVE_MEMBERS("Failed to retrieve members", HttpStatus.INTERNAL_SERVER_ERROR),
+    FAILED_TO_REGISTER_MEMBER("Failed to register member", HttpStatus.INTERNAL_SERVER_ERROR),
+    FAILED_TO_CHANGE_PASSWORD("Failed to change password", HttpStatus.INTERNAL_SERVER_ERROR),
+    FAILED_TO_DEACTIVATE_MEMBER("Failed to deactivate member", HttpStatus.INTERNAL_SERVER_ERROR),
+    VALIDATION_FAILED("Validation failed", HttpStatus.BAD_REQUEST),
+    VALIDATION_PW_FAILED("Validation password failed", HttpStatus.BAD_REQUEST);
 
-    private final String code;
     private final String message;
+    private final HttpStatus status;
 
-    ApiMessage(String code, String message) {
-        this.code = code;
+    ApiMessage(String message, HttpStatus status) {
         this.message = message;
+        this.status = status;
     }
 }
+
 
